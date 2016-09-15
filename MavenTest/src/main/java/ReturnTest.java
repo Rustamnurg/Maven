9d3ff5bf15c28e454d90a00c77f3e74dad883a86
Rustamnurg <rustik6030@gmail.com>
2016-09-15 23:07:19 +0300

    TestMaven

---
+++ MavenTest/src/main/java/ReturnTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rustam on 15/09/16.
+ */
+public class ReturnTest {
+
+    public  void test() {
+        System.out.println("sd");
+
+    }
+}
---
+++ MavenTest2/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Rustam on 15/09/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ReturnTest returnTest = new ReturnTest();
+        returnTest.test();
+    }
+}
